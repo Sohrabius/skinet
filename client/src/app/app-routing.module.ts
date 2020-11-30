94aafcdaf2931dad5fdf8dc0e28c46f2ef2c1fed
Mostafa <Sohrabius@users.noreply.github.com>
2020-11-30 10:31:59 +0330

    round 21

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { AuthGuard } from './core/guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
-  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
-  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
-  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'Basket'}},
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  { path: 'test-error', component: TestErrorComponent, data: { breadcrumb: 'Test Errors' } },
+  { path: 'server-error', component: ServerErrorComponent, data: { breadcrumb: 'Server Error' } },
+  { path: 'not-found', component: NotFoundComponent, data: { breadcrumb: 'Not Found' } },
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: { breadcrumb: 'Shop' } },
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: { breadcrumb: 'Basket' } },
   {
     path: 'checkout',
     canActivate: [AuthGuard],
@@ -20,10 +20,18 @@ const routes: Routes = [
       .then(mod => mod.CheckoutModule), data: { breadcrumb: 'Checkout' }
   },
   {
-    path: 'account', loadChildren: () => import('./account/account.module')
+    path: 'orders',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./orders/orders.module')
+      .then(mod => mod.OrdersModule), data: { breadcrumb: 'Orders' }
+  },
+  {
+    path: 'account',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./account/account.module')
       .then(mod => mod.AccountModule), data: { breadcrumb: { skip: true } }
   },
-  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
 @NgModule({
